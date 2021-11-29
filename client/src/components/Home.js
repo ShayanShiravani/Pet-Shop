3fea6adb2244002acc3836eb7d65e74c427dd79c
Shayan Shiravani <Shayan5420@yahoo.com>
2021-11-29 19:49:42 +0330

    Remove useless constructor

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
+import "../css/Home.css";
 
 class Home extends Component {
-
-    constructor(props) {
-        super(props);
-    }
-
+    
     render() {
         return (
             <div>
                 <br/>
                 <div className="jumbotron">
-                    <h1 className="display-4">Hello, world!</h1>
-                    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <h1 className="display-4">Welcome to Shayan's PetShop</h1>
+                    <br/>
+                    <h5 className="pet-quote"><q>A dog is the only thing on earth that loves you more than he loves himself.</q>__Josh Billings</h5>
+                    <p className="lead">My decentralize PetShop implemented using Ethereum smart contracts</p>
                     <hr className="my-4" />
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p>Please feel free to fork it or do whatever you want with it.</p>
                     <p className="lead">
                         <a className="btn btn-primary btn-lg" 
                         href="https://github.com/ShayanShiravani/Pet-Shop.git" role="button">GitHub</a>
