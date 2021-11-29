9d309ef34153f239bb46152fa7b1c70d5e3bfedc
Shayan Shiravani <Shayan5420@yahoo.com>
2021-11-29 09:55:52 +0330

    Change homepage layout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Branch from "./Branch";
 
 class Home extends Component {
 
@@ -10,22 +11,70 @@ class Home extends Component {
     }
 
     componentDidMount = async () => {
-        // await this.props.client.contracts.PetShop.methods
-        // .addBranch('0x3B83AeD275216625fbbE8DB71E0478C28f1405F9')
-        // .send({ from: this.props.client.activeAccount });
-
         const response = await this.props.client.contracts.PetShop.methods.getBranches().call();
-        console.log(response);
         this.setState({ branchesList: response });
     }
 
+    createBranchesList = (list) => {
+
+    };
+
     render() {
+        if(this.state.branchesList != null)
+        {
+            const branchAddresses = this.state.branchesList[1];
+            var firstColList = branchAddresses.map((address, index) => {
+                if(index%2 === 0 && 
+                    address !== '0x0000000000000000000000000000000000000000')
+                {
+                    return <Branch key={index} title="test" address={address}/>;
+                }
+                return null;
+            });
+            var secondColList = branchAddresses.map((address, index) => {
+                if(index%2 === 1 && 
+                    address !== '0x0000000000000000000000000000000000000000')
+                {
+                    return <Branch key={index} title="test" address={address}/>
+                }
+                return null;
+            });
+        }
         return (
-            <div className="row d-flex justify-content-center">
-                Welcome to Shayan's Pet Shop
-                <div className="branches-list">
-                    
+            <div>
+                <br/>
+                <div className="jumbotron">
+                    <h1 className="display-4">Hello, world!</h1>
+                    <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr className="my-4" />
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p className="lead">
+                        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    </p>
                 </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <h3>Column 1</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                        </div>
+                        <div className="col-sm-4">
+                            <h3>Column 2</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                        </div>
+                        <div className="col-sm-4">
+                            <h3>Column 3</h3> 
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className="row justify-content-center">
+                    <div className="col-md-4">{firstColList}</div>
+                    <div className="col-md-4">{secondColList}</div>
+                </div> */}
             </div>
         );
     }
