7b99b66152339ef8d760f888591279a8a4452c31
Shayan Shiravani <Shayan5420@yahoo.com>
2021-11-29 09:57:16 +0330

    Change navigation bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,31 +4,42 @@ import "../css/Header.css";
 class Header extends Component {
     render() {
         return (
-            <nav className="nav">
-                <div className="container-fluid">
-                    <div className="logo">
-                        <a href="/">Shayan's Pet-Shop</a>
-                    </div>
-                    <div id="mainListDiv" className="main_list">
-                        <ul className="navlinks">
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/contact">Contact</a></li>
-                            <li>
-                                <form className="form-inline">
-                                    <input className="form-control form-control-lg mr-sm-2" type="search"
-                                    placeholder="Search" aria-label="Search" />
-                                    <button className="btn btn-lg btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                    <span className="navTrigger">
-                        <i></i>
-                        <i></i>
-                        <i></i>
-                    </span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Shayan's Pet-Shop</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/add-branch">Add branch</a>
+                        </li>
+                        {/* <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Disabled</a>
+                        </li> */}
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
-            </nav>
+                </nav>
         );
     }
 }
