107369143991e0d924610e7e246851ff01742ae5
Shayan Shiravani <Shayan5420@yahoo.com>
2021-11-29 09:58:23 +0330

    Change branch display style

--- client/src/components/Branch.js
+++ client/src/components/Branch.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../css/Branch.css";
 
 class Branch extends Component {
 
@@ -8,21 +9,19 @@ class Branch extends Component {
 
     render() {
         return (
-            <div>
-                <div className="col-sm-6 col-md-4 col-lg-3">
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">{this.props.title}</h3>
-                        </div>
-                        <div class="panel-body">
-                            {/* <img alt="140x140" data-src="holder.js/140x140" class="img-rounded img-center" style="width: 100%;" src="https://animalso.com/wp-content/uploads/2017/01/Golden-Retriever_6.jpg" data-holder-rendered="true" /> */}
-                            {/* <br/><br/> */}
-                            <strong>Address</strong>: <span>{this.props.address}</span><br/>
-                            {/* <strong>Age</strong>: <span className="pet-age">3</span><br/>
-                            <strong>Location</strong>: <span className="pet-location">Warren, MI</span><br/><br/> */}
-                            <button class="btn btn-default" type="button" data-id="0">View details</button>
-                        </div>
+            <div className="card">
+                <div className="card-header">
+                    {this.props.title}
+                </div>
+                <div className="card-body">
+                    <div className="branch-img">
+                        <img src="/logo192.png" className="card-img-top" alt="" />
                     </div>
+                    <h5 className="card-title">{this.props.title}</h5>
+                    <p className="card-text">{this.props.address}</p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted"></small>
                 </div>
             </div>
         );
