6f29f9ea42672f2b485ef2c5a7fd337f3eff60b2
Shayan Shiravani <Shayan5420@yahoo.com>
2021-12-02 00:24:15 +0330

    Change define branch

--- client/src/components/AddBranch.js
+++ client/src/components/AddBranch.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../css/AddBranch.css";
+import Common from "../objects/Common";
 
 class AddBranch extends Component {
 
@@ -7,6 +8,7 @@ class AddBranch extends Component {
         super(props);
         this.state = {
             branchAddress: '',
+            branchTitle: ''
         };
     }
 
@@ -24,32 +26,54 @@ class AddBranch extends Component {
 
     submitForm = (e) => {
         e.preventDefault();
-        this.addBranch(this.state.branchAddress);
+        this.addBranch(this.state.branchAddress, this.state.branchTitle);
+        return false;
     }
 
     handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
     }
 
-    addBranch = async (address) => {
+    addBranch = async (address, title) => {
         if(address.trim() === '')
         {
             alert("Please enter an account address");
             return false;
         }
-        await this.props.client.contracts.PetShop.methods.addBranch(address)
+        if(Common.isEmpty(title))
+        {
+            alert("Please enter a title");
+            return false;
+        }
+        await this.props.client.contracts.PetShop.methods.addBranch(address, title)
         .send({ from: this.props.client.activeAccount });
     }
 
     render() {
         return (
-            <div className="row add-branch m-3">
-                <div className="col-md-5">
-                    <form noValidate onSubmit={this.submitForm}>
-                        <label htmlFor="branch_address" className="form-label">Branch address</label>
-                        <input type="text" name="branchAddress" className="form-control form-control-lg" id="branch_address" value={this.state.branchAddress} onChange={this.handleChange} />
-                        <input type="submit" className="btn btn-lg btn-outline-warning btn-block mt-4" value="Add" />
-                    </form>
+            <div className="modal fade" id="add_branch" tabIndex="-1" role="dialog" aria-labelledby="add_branch_label" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="add_branch_label">Define branch</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <form noValidate onSubmit={this.submitForm}>
+                            <label htmlFor="branch_address" className="form-label">address</label>
+                            <input type="text" name="branchAddress" className="form-control form-control-lg" id="branch_address" value={this.state.branchAddress} onChange={this.handleChange} />
+                            <label htmlFor="branch_title" className="form-label">title</label>
+                            <input type="text" name="branchTitle" className="form-control form-control-lg" id="branch_title" value={this.state.branchTitle} onChange={this.handleChange} />
+                            <input type="submit" className="btn btn-lg btn-outline-warning btn-block mt-4" value="Add" />
+                        </form>
+                    </div>
+                    <div className="modal-footer">
+                        {/* <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button> */}
+                    </div>
+                    </div>
                 </div>
             </div>
         );
